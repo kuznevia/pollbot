d6c50a879ca7559887e9d51d36cf2f1fa369f3b9
kuznevia <via.kuznetsov@yandex.ru>
2024-11-08 16:02:56 +0300

    fix cron logic

--- src/features/poll/games/games.ts
+++ src/features/poll/games/games.ts
@@ -2,6 +2,7 @@ import TelegramBot from 'node-telegram-bot-api';
 import cron from 'node-cron';
 import { sendGamePoll } from './poll';
 import { chatId } from '../../../shared/consts/consts';
+import { checkGameTomorrow } from './utils/checkGameTommorow';
 
 export const startGamePollListener = (bot: TelegramBot) => {
   // Команда для запуска опроса
@@ -13,8 +14,13 @@ export const startGamePollListener = (bot: TelegramBot) => {
 export const scheduleGamePoll = (bot: TelegramBot) => {
   // Периодическая проверка расписания
   // Каждое утро в 11:00 мск
-  cron.schedule('0 11 * * *', () => {
-    console.log('Пробую автоматически отправить опрос на игру');
-    sendGamePoll(bot, Number(chatId));
+  cron.schedule('0 11 * * *', async () => {
+    const tommorrowGames = await checkGameTomorrow();
+    if (tommorrowGames.length) {
+      console.log('Пробую автоматически отправить опрос на игру');
+      sendGamePoll(bot, Number(chatId));
+    } else {
+      console.log('Завтра нет игр');
+    }
   });
 };
